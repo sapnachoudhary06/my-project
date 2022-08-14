feb12937212c5f07861dd139d011e706a8fea27e
Nikhil Kumar <nkhlneil@gmail.com>
2022-08-14 09:41:16 +0200

    Added the function logic

---
+++ countVowels.js
@@ -0,0 +1,14 @@
+function countVowels(str) {
+  let vowels = 'aeiou';
+  let count = 0;
+
+  for (let x of str) {
+    if (vowels.includes(x)) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowels('Sapnaaaaaaawiui'));
